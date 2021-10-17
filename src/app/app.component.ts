7cb982f9acf6de99c989645945f24e843ce08467
AlexxxKRSK <C3i9vic1!>
2021-10-17 20:50:54 +0700

    v.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from "@angular/router";
+import {UserService} from "./user/user.service";
+import {Observable} from "rxjs";
+import {User} from "./entities/user";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WebClient';
+  users$: Observable<User[]>;
+
+  constructor(private router: Router,
+              private service: UserService) {
+    this.users$ = this.service.findAll();
+  }
+
+  public navigateToUsers(): void {
+    this.router.navigate(['./user']);
+  }
+
+  navigateToMain() {
+    this.router.navigate([""]);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {UserComponent} from './user/user/user.component';
+import {UserService} from "./user/user.service";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatListModule} from "@angular/material/list";
+import {MatDialogModule} from "@angular/material/dialog";
+import { EditUserFormComponent } from './user/edit-user-form/edit-user-form.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    EditUserFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatDialogModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    UserService,
+    HttpClient,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
